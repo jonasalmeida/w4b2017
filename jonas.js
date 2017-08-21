dcfdc74c0b489796f56a8163f3b8669ec0f22ef6
Jonas Almeida <jalmeida@mathbiol.org>
2017-08-21 16:08:45 -0400

    addition working

--- jonas.js
+++ jonas.js
@@ -1,3 +1,17 @@
 console.log('hello from Jonas at ' + Date())
 
-lala = 99
+lala = 999
+
+// z = x + y
+
+/*
+parseFloat(x.value)
+parseFloat(x.value)
+*/
+
+x.size=y.size=10
+
+x.onkeyup=y.onkeyup=function(ev){
+    //console.log('you pressed x :',this)
+    z.textContent=parseFloat(x.value)+parseFloat(y.value)
+}
