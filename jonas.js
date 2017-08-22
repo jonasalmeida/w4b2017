2b13c7b8670dcd5ca8c2f3cdb554f6f20bc1a0e0
Jonas Almeida <jalmeida@mathbiol.org>
2017-08-22 12:25:42 -0400

    list displayed

--- jonas.js
+++ jonas.js
@@ -16,4 +16,30 @@ x.onkeyup=y.onkeyup=function(ev){
     z.textContent=parseFloat(x.value)+parseFloat(y.value)
 }
 
-cbio.get()
+listCancers=function(id){
+    if(!id){console.warn('id not defined')
+    }else{
+        cbio.get(
+            null,
+            function(x){
+                var div = document.getElementById(id)
+                var h = ''
+                x.forEach(function(xi,i){
+                   if(i==0){
+                       h +='<h2>'+xi+'</h2>'
+                   }else{
+                       h +='<li>'+xi+'</li>'
+                   }
+
+                   //debugger 
+                })
+                div.innerHTML=h
+                
+            }
+        )    
+    }
+     
+    
+}
+
+listCancers('cancerTypesDiv')
